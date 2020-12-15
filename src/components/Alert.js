aac7b8aee6d2205f78b8be2efe1ab7c2505fdefa
Kimberly Diehl <kimberly783@gmail.com>
2020-12-15 15:41:49 -0500

    End Game Scoreboard start

---
+++ src/components/Alert.js
@@ -0,0 +1,17 @@
+// import React, { useContext } from "react";
+// import Alert from "react-bootstrap/Alert";
+
+// const Alerts = () => {
+//   const alertContext = useContext(Alert);
+
+//   return (
+//     alertContext.alerts.length > 0 &&
+//     alertContext.alerts.map((alert, index) => (
+//       <Alert key={alert.id} id={index} severity="error">
+//         {alert.msg}
+//       </Alert>
+//     ))
+//   );
+// };
+
+// export default Alerts;
