9dc48eaef6a4d89dd801d438208e48163816b8b9
Jay Villari <jay.villari12@gmail.com>
2020-12-16 16:16:36 -0500

    fix api call for definition key

--- src/components/Api.js
+++ src/components/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const wordAPI = "https://random-word-api.herokuapp.com/word?number=10";
 const definitionAPI = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"; //voluminous?key=your-api-key"
-const definitionKey = REACT_APP_MW_API_KEY;
+const definitionKey = process.env.REACT_APP_MW_API_KEY;
 let definitions = [];
 function getWords() 
 {
